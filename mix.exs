b9f4ab08f0ced35e2321a874e66f692e94c47f92
Bryan Finlayson <bdfinlayson@gmail.com>
2018-02-10 16:14:03 -0600

    add poison support

--- mix.exs
+++ mix.exs
@@ -22,7 +22,8 @@ defmodule RepoDeleter.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:httpoison, "~> 1.0"}
+      {:httpoison, "~> 1.0"},
+      {:poison, "~> 3.1"}
     ]
   end
 end
